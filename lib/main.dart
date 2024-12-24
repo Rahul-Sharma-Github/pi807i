276718abc05453147338f24025be33315c98885e
Rahul <rahulsharma3258work@gmail.com>
2024-12-24 16:29:27 +0530

    initialized Firebase in main function before app launch |

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pi807i/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app/features/home_page/view/home_page.dart';
 
-void main() {
+// it will initialize our Flutter Firebase app before starting the App
+
+Future<void> main() async {
+  // this is to make sure that the WidgetsBinding is initialized before calling Firebase.initializeApp()
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Root Widget of the app
   runApp(const MyApp());
 }
 
